0829dff4f0fe051cbb34299805f6935c7c72bf7f
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-09-02 16:23:41 +0100

    Added basic sonar qube stuff

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+if [ ! -z "$SONARQUBE_JDBC_PASSWORD_PATH" ]; then
+    SONARQUBE_JDBC_PASSWORD="$(cat $SONARQUBE_JDBC_PASSWORD_PATH)"
+    export SONARQUBE_JDBC_PASSWORD
+fi
+
+if [ "${1:0:1}" != '-' ]; then
+	exec "$@"
+fi
+
+exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
+	"$@"
+

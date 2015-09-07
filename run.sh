e717ef49aeaa7136c9291554b521a94cff8e5d4c
Billie Alice Thompson <billie@purplebooth.co.uk>
2015-09-07 16:29:05 +0100

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -15,20 +15,20 @@ if [ "${1:0:1}" != '-' ]; then
 	exec "$@"
 fi
 
-./restore.sh
+"$SONARQUBE_HOME/bin/restore.sh"
 
-EXITCODE=0
-
-if [ ! java -jar "lib/sonar-application-$SONAR_VERSION.jar" \
+set +e
+java -jar "lib/sonar-application-$SONAR_VERSION.jar" \
   -Dsonar.log.console=true \
   -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
   -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
   -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
   -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
-    "$@" ] ; then
-    EXITCODE=$?
-fi
+    "$@"
+set -e
 
-./backup.sh
+RETURN_CODE=$?
 
-exit ${EXITCODE}
+"$SONARQUBE_HOME/bin/backup.sh"
+
+exit ${RETURN_CODE}

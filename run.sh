aca64d63162cab750527783aaa6317e9a96afe6c
Jay Keshur <jay.keshur@digital.homeoffice.gov.uk>
2017-03-08 12:01:43 +0000

    Update run to include sonar args

--- run.sh
+++ run.sh
@@ -6,24 +6,34 @@ mkdir -p extensions/jdbc-driver/oracle/ \
          extensions/deprecated \
          extensions/plugins
 
+SONAR_ARGS='-Dsonar.web.javaOpts=-server -Dsonar.log.console=true'
+
 if [ ! -z "$SONARQUBE_JDBC_PASSWORD_PATH" ]; then
-    SONARQUBE_JDBC_PASSWORD="$(cat "$SONARQUBE_JDBC_PASSWORD_PATH")"
-    export SONARQUBE_JDBC_PASSWORD
+  SONARQUBE_JDBC_PASSWORD="$(cat "$SONARQUBE_JDBC_PASSWORD_PATH")"
+  export SONARQUBE_JDBC_PASSWORD
+fi
+
+if [ -z "$SONARQUBE_JDBC_USERNAME" ]; then
+  SONAR_ARGS="${SONAR_ARGS} -Dsonar.jdbc.username=\"$SONARQUBE_JDBC_USERNAME\""
+fi
+
+if [ -z "$SONARQUBE_JDBC_PASSWORD" ]; then
+  SONAR_ARGS="${SONAR_ARGS} -Dsonar.jdbc.password=\"$SONARQUBE_JDBC_PASSWORD\""
+fi
+
+if [ -z "$SONARQUBE_JDBC_URL" ]; then
+  SONAR_ARGS="${SONAR_ARGS} -Dsonar.jdbc.url=\"$SONARQUBE_JDBC_URL\""
+else
+  SONAR_ARGS="${SONAR_ARGS} -Dsonar.embeddedDatabase.port=9092"
 fi
 
 if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
+echo "Running sonar with the following args: ${SONAR_ARGS}"
 EXITCODE=0
 
-java -jar "lib/sonar-application-$SONAR_VERSION.jar" \
-  -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  "${SONARQUBE_OPTS}" \
-  -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
-  "$@"
+java -jar "lib/sonar-application-$SONAR_VERSION.jar" -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" ${SONAR_ARGS} "$@"
 
 exit ${EXITCODE}

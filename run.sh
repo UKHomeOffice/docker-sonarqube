b766ff58bf1d3ca2b0982b50b433be19c8905ce1
Onur Yelekci <onur.yelekci@digital.homeoffice.gov.uk>
2020-02-16 16:52:29 +0000

    Use postgresql adapter with v6.7.6

--- run.sh
+++ run.sh
@@ -1,44 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set +e
-chown -R sonarqube:sonarqube $SONARQUBE_HOME
 set -e
-
-# Ensure that the plugins directories are created if we're mounting a fresh volume
-mkdir -p extensions/jdbc-driver/oracle/ \
-         extensions/deprecated \
-         extensions/plugins
-
-SONAR_ARGS='-Dsonar.web.javaOpts=-server -Dsonar.log.console=true'
-
-if [ ! -z "$SONARQUBE_JDBC_PASSWORD_PATH" ]; then
-  SONARQUBE_JDBC_PASSWORD="$(cat "$SONARQUBE_JDBC_PASSWORD_PATH")"
-  export SONARQUBE_JDBC_PASSWORD
-fi
-
-if [ ! -z "$SONARQUBE_JDBC_USERNAME" ]; then
-  SONAR_ARGS="${SONAR_ARGS} -Dsonar.jdbc.username=$SONARQUBE_JDBC_USERNAME"
-fi
-
-if [ ! -z "$SONARQUBE_JDBC_PASSWORD" ]; then
-  SONAR_ARGS="${SONAR_ARGS} -Dsonar.jdbc.password=$SONARQUBE_JDBC_PASSWORD"
-fi
-
-if [ ! -z "$SONARQUBE_JDBC_URL" ]; then
-  SONAR_ARGS="${SONAR_ARGS} -Dsonar.jdbc.url=$SONARQUBE_JDBC_URL"
-fi
-
-if [[ "${SONARQUBE_JDBC_URL}" != *"mysql"* ]]; then
-  SONAR_ARGS="${SONAR_ARGS} -Dsonar.embeddedDatabase.port=9092"
-fi
 
 if [ "${1:0:1}" != '-' ]; then
   exec "$@"
 fi
 
-echo "Running sonar with the following args: ${SONAR_ARGS}"
-EXITCODE=0
+# Parse Docker env vars to customize SonarQube
+#
+# e.g. Setting the env var sonar.jdbc.username=foo
+#
+# will cause SonarQube to be invoked with -Dsonar.jdbc.username=foo
 
-java -jar "lib/sonar-application-$SONAR_VERSION.jar" -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" ${SONAR_ARGS} "$@"
+declare -a sq_opts
 
-exit ${EXITCODE}
+while IFS='=' read -r envvar_key envvar_value
+do
+    if [[ "$envvar_key" =~ sonar.* ]] || [[ "$envvar_key" =~ ldap.* ]]; then
+        sq_opts+=("-D${envvar_key}=${envvar_value}")
+    fi
+done < <(env)
+
+exec tail -F ./logs/es.log & # this tail on the elasticsearch logs is a temporary workaround, see https://github.com/docker-library/official-images/pull/6361#issuecomment-516184762
+exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
+  -Dsonar.log.console=true \
+  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+  -Dsonar.web.javaAdditionalOpts="$SONARQUBE_WEB_JVM_OPTS -Djava.security.egd=file:/dev/./urandom" \
+  "${sq_opts[@]}" \
+  "$@"
